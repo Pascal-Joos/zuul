98b3fe09ac550855de7990411638386ea66e4150
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 08:54:49 +0000

    Revert "fix: 13 - DEREFERENCE_NULLABLE - return new InetSocketAddress(bindType.addressSupplier.get(), port); - dereferenced expression bindType.addressSupplier is @Nullable"
    This reverts commit d7dd139d93682d9bb47baab303230cd984464b43.

--- zuul-core/src/main/java/com/netflix/zuul/netty/server/SocketAddressProperty.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/server/SocketAddressProperty.java
@@ -148,9 +148,7 @@ public final class SocketAddressProperty extends StringDerivedProperty<SocketAdd
         case ANY_LOCAL: // fallthrough
         case IPV4_LOCAL: // fallthrough
         case IPV6_LOCAL: // fallthrough
-          return new InetSocketAddress(
-              java.util.Objects.requireNonNull(bindType.addressSupplier, "addressSupplier").get(),
-              port);
+          return new InetSocketAddress(bindType.addressSupplier.get(), port);
         case UDS:
           return new DomainSocketAddress(rawAddress);
       }
