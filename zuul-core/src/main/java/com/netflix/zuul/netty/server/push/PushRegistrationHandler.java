424f3aaf9326c381c6f6b71701873199b5d98948
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 16:37:50 +0000

    fix: Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull field ctx (line 44) is initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/vscode/nullness-benchmarks/zuul/zuul-core/src/main/java/com/netflix/zuul/netty/server/push/PushRegistrationHandler.java:63   public PushRegistrationHandler(

--- zuul-core/src/main/java/com/netflix/zuul/netty/server/push/PushRegistrationHandler.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/server/push/PushRegistrationHandler.java
@@ -41,7 +41,7 @@ public class PushRegistrationHandler extends ChannelInboundHandlerAdapter {
 
   /* state */
   protected final AtomicBoolean destroyed;
-  private ChannelHandlerContext ctx;
+  @Nullable private ChannelHandlerContext ctx;
   @Nullable private volatile PushConnection pushConnection;
   private final List<ScheduledFuture<?>> scheduledFutures;
 
@@ -106,7 +106,9 @@ public class PushRegistrationHandler extends ChannelInboundHandlerAdapter {
   protected final void forceCloseConnectionFromServerSide() {
     if (!destroyed.get()) {
       logger.debug("server forcing close connection");
-      pushProtocol.sendErrorAndClose(ctx, 1000, "Server closed connection");
+      if (ctx != null) {
+        pushProtocol.sendErrorAndClose(ctx, 1000, "Server closed connection");
+      }
     }
   }
 
@@ -120,7 +122,7 @@ public class PushRegistrationHandler extends ChannelInboundHandlerAdapter {
   }
 
   private void requestClientToCloseConnection() {
-    if (ctx.channel().isActive()) {
+    if (ctx != null && ctx.channel().isActive()) {
       // Application level protocol for asking client to close connection
       ctx.writeAndFlush(pushProtocol.goAwayMessage());
       // Force close connection if client doesn't close in reasonable time after we made request
@@ -136,7 +138,7 @@ public class PushRegistrationHandler extends ChannelInboundHandlerAdapter {
   }
 
   protected void keepAlive() {
-    if (KEEP_ALIVE_ENABLED.get()) {
+    if (KEEP_ALIVE_ENABLED.get() && ctx != null) {
       ctx.writeAndFlush(new PingWebSocketFrame());
     }
   }
