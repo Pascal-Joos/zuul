69c1996431b3acc9dc5cf4184bbe757a4085c7c5
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 16:49:31 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression cause.getMessage() is @Nullable' /home/vscode/nullness-benchmarks/zuul/zuul-core/src/main/java/com/netflix/zuul/netty/server/ssl/SslHandshakeInfoHandler.java:138               && cause.getMessage().contains("failure when writing TLS control frames")) {

--- zuul-core/src/main/java/com/netflix/zuul/netty/server/ssl/SslHandshakeInfoHandler.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/server/ssl/SslHandshakeInfoHandler.java
@@ -139,6 +139,7 @@ public class SslHandshakeInfoHandler extends ChannelInboundHandlerAdapter {
                 clientIP,
                 ChannelUtils.channelInfoForLogging(ctx.channel()));
           } else if (cause instanceof SSLException
+              && cause.getMessage() != null
               && cause.getMessage().contains("failure when writing TLS control frames")) {
             // This can happen if the ClientHello is sent followed  by a RST packet, before we can
             // respond.
