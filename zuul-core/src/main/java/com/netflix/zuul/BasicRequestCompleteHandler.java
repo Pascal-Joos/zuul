aba72574c0bcb5dc212e0d7021ca10281dbccae5
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 08:23:17 +0000

    Revert "fix: 1 - DEREFERENCE_NULLABLE - SessionContext context = inboundRequest.getContext(); - dereferenced expression inboundRequest is @Nullable"
    This reverts commit 6b8cc418d5a1298b8310f90c4a408447039ef72b.

--- zuul-core/src/main/java/com/netflix/zuul/BasicRequestCompleteHandler.java
+++ zuul-core/src/main/java/com/netflix/zuul/BasicRequestCompleteHandler.java
@@ -30,9 +30,6 @@ public class BasicRequestCompleteHandler implements RequestCompleteHandler {
   @Override
   public void handle(
       @Nullable HttpRequestInfo inboundRequest, @Nullable HttpResponseMessage response) {
-    if (inboundRequest == null) {
-      return;
-    }
     SessionContext context = inboundRequest.getContext();
 
     // Publish request-level metrics.
