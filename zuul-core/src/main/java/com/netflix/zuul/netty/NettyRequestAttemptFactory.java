9be69e56053cfe18bdfaf293f62f541d49786d41
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 08:36:39 +0000

    Revert "fix: 6 - DEREFERENCE_NULLABLE - if (cause instanceof IllegalStateException && cause.getMessage().contains("server")) { - dereferenced expression cause.getMessage() is @Nullable"
    This reverts commit 7932538401b3ad5264cb0247a010358c976ad49c.

--- zuul-core/src/main/java/com/netflix/zuul/netty/NettyRequestAttemptFactory.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/NettyRequestAttemptFactory.java
@@ -68,9 +68,7 @@ public class NettyRequestAttemptFactory {
     }
 
     final Throwable cause = t.getCause();
-    if (cause instanceof IllegalStateException
-        && cause.getMessage() != null
-        && cause.getMessage().contains("server")) {
+    if (cause instanceof IllegalStateException && cause.getMessage().contains("server")) {
       LOG.warn("IllegalStateException mapped to NO_AVAILABLE_SERVERS", cause);
       return NO_AVAILABLE_SERVERS;
     }
