4464ee3c89e00d471e5f838eb66200d9234dd75a
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 08:34:02 +0000

    Revert "fix: 5 - WRONG_OVERRIDE_RETURN - @Nullable - method returns @Nullable, but superclass method com.netflix.zuul.message.http.HttpResponseInfo.getInboundRequest() returns @NonNull"
    This reverts commit 2361b80c2566f3024bc93c00fd13da075fd302a1.

--- zuul-core/src/main/java/com/netflix/zuul/message/http/HttpResponseMessageImpl.java
+++ zuul-core/src/main/java/com/netflix/zuul/message/http/HttpResponseMessageImpl.java
@@ -27,7 +27,6 @@ import io.netty.handler.codec.http.Cookie;
 import io.netty.handler.codec.http.CookieDecoder;
 import io.netty.handler.codec.http.HttpContent;
 import io.netty.handler.codec.http.ServerCookieEncoder;
-import java.util.Objects;
 import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -154,9 +153,10 @@ public class HttpResponseMessageImpl implements HttpResponseMessage {
     message.disposeBufferedBody();
   }
 
+  @Nullable
   @Override
   public HttpRequestInfo getInboundRequest() {
-    return Objects.requireNonNull(outboundRequest.getInboundRequest());
+    return outboundRequest.getInboundRequest();
   }
 
   @Override
