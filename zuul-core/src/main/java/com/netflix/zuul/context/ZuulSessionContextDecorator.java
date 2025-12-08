ec035643482eb6a2b33b81d7aedc3ff6dd158a30
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 08:32:08 +0000

    Revert "fix: 4 - WRONG_OVERRIDE_RETURN - @Nullable - method returns @Nullable, but superclass method com.netflix.zuul.context.SessionContextDecorator.decorate(com.netflix.zuul.context.SessionContext) returns @NonNull"
    This reverts commit 78b0771adfe3e607215e77cac847d8eda04f838f.

--- zuul-core/src/main/java/com/netflix/zuul/context/ZuulSessionContextDecorator.java
+++ zuul-core/src/main/java/com/netflix/zuul/context/ZuulSessionContextDecorator.java
@@ -24,6 +24,7 @@ import com.netflix.zuul.origins.OriginManager;
 import com.netflix.zuul.passport.CurrentPassport;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -44,6 +45,7 @@ public class ZuulSessionContextDecorator implements SessionContextDecorator {
     this.originManager = originManager;
   }
 
+  @Nullable
   @Override
   public SessionContext decorate(SessionContext ctx) {
     // TODO split out commons parts from BaseSessionContextDecorator
@@ -51,7 +53,7 @@ public class ZuulSessionContextDecorator implements SessionContextDecorator {
     ChannelHandlerContext nettyCtx =
         (ChannelHandlerContext) ctx.get(CommonContextKeys.NETTY_SERVER_CHANNEL_HANDLER_CONTEXT);
     if (nettyCtx == null) {
-      return ctx;
+      return null;
     }
 
     Channel channel = nettyCtx.channel();
