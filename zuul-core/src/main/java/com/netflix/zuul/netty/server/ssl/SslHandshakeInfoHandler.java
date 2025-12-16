2a0251fff97f2f0b59b918ec3c00f57739f5292d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:13:43 +0000

    fix: 18 - DEREFERENCE_NULLABLE - && cause.getMessage().contains("failure when writing TLS control frames")) { - dereferenced expression cause.getMessage() is @Nullable

--- zuul-core/src/main/java/com/netflix/zuul/netty/server/ssl/SslHandshakeInfoHandler.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/server/ssl/SslHandshakeInfoHandler.java
@@ -129,12 +129,16 @@ public class SslHandshakeInfoHandler extends ChannelInboundHandlerAdapter {
                 clientIP,
                 ChannelUtils.channelInfoForLogging(ctx.channel()));
           } else if (cause instanceof SSLException
+              && cause != null
+              && cause.getMessage() != null
               && cause.getMessage().contains("handshake timed out")) {
             logger.debug(
                 "Client timed-out doing the ssl handshake. , client_ip = {}, channel_info = {}",
                 clientIP,
                 ChannelUtils.channelInfoForLogging(ctx.channel()));
           } else if (cause instanceof SSLException
+              && cause != null
+              && cause.getMessage() != null
               && cause.getMessage().contains("failure when writing TLS control frames")) {
             // This can happen if the ClientHello is sent followed  by a RST packet, before we can
             // respond.
