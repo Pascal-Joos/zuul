3b7a5d0c5cafaf493585f9592398f2967ac2f7f6
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:11:45 +0000

    Revert "fix: 17 - DEREFERENCE_NULLABLE - && cause.getMessage().contains("handshake timed out")) { - dereferenced expression cause.getMessage() is @Nullable"
    This reverts commit b4a88c13a7a9b52aaf9e1ec187cbac6f20e68000.

--- zuul-core/src/main/java/com/netflix/zuul/netty/server/ssl/SslHandshakeInfoHandler.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/server/ssl/SslHandshakeInfoHandler.java
@@ -113,7 +113,6 @@ public class SslHandshakeInfoHandler extends ChannelInboundHandlerAdapter {
           String clientIP =
               ctx.channel().attr(SourceAddressChannelHandler.ATTR_SOURCE_ADDRESS).get();
           Throwable cause = sslEvent.cause();
-          String causeMessage = (cause != null) ? cause.getMessage() : null;
 
           PassportState passportState = CurrentPassport.fromChannel(ctx.channel()).getState();
           if (cause instanceof ClosedChannelException
@@ -130,15 +129,13 @@ public class SslHandshakeInfoHandler extends ChannelInboundHandlerAdapter {
                 clientIP,
                 ChannelUtils.channelInfoForLogging(ctx.channel()));
           } else if (cause instanceof SSLException
-              && causeMessage != null
-              && causeMessage.contains("handshake timed out")) {
+              && cause.getMessage().contains("handshake timed out")) {
             logger.debug(
                 "Client timed-out doing the ssl handshake. , client_ip = {}, channel_info = {}",
                 clientIP,
                 ChannelUtils.channelInfoForLogging(ctx.channel()));
           } else if (cause instanceof SSLException
-              && causeMessage != null
-              && causeMessage.contains("failure when writing TLS control frames")) {
+              && cause.getMessage().contains("failure when writing TLS control frames")) {
             // This can happen if the ClientHello is sent followed  by a RST packet, before we can
             // respond.
             logger.debug(
