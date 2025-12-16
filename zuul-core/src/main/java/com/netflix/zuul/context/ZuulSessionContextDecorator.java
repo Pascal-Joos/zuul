c27df049910b4f3e44fcaf9c6a2b60918125bd8c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:57:04 +0000

    fix: 4 - WRONG_OVERRIDE_RETURN - @Nullable - method returns @Nullable, but superclass method com.netflix.zuul.context.SessionContextDecorator.decorate(com.netflix.zuul.context.SessionContext) returns @NonNull

--- zuul-core/src/main/java/com/netflix/zuul/context/ZuulSessionContextDecorator.java
+++ zuul-core/src/main/java/com/netflix/zuul/context/ZuulSessionContextDecorator.java
@@ -53,7 +53,7 @@ public class ZuulSessionContextDecorator implements SessionContextDecorator {
     ChannelHandlerContext nettyCtx =
         (ChannelHandlerContext) ctx.get(CommonContextKeys.NETTY_SERVER_CHANNEL_HANDLER_CONTEXT);
     if (nettyCtx == null) {
-      return null;
+      return ctx;
     }
 
     Channel channel = nettyCtx.channel();
