5d16c708200e397c0014105f894f38d1b61a56cc
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:07:35 +0000

    Revert "fix: 14 - METHOD_NO_INIT - public PushRegistrationHandler( - initializer method does not guarantee @NonNull field ctx (line 44) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit e55cad633061daf383ac519959fd8ce37dbbeaf0.

--- zuul-core/src/main/java/com/netflix/zuul/netty/server/push/PushRegistrationHandler.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/server/push/PushRegistrationHandler.java
@@ -62,8 +62,8 @@ public class PushRegistrationHandler extends ChannelInboundHandlerAdapter {
 
   public PushRegistrationHandler(
       PushConnectionRegistry pushConnectionRegistry, PushProtocol pushProtocol) {
-    this.pushConnectionRegistry = Objects.requireNonNull(pushConnectionRegistry);
-    this.pushProtocol = Objects.requireNonNull(pushProtocol);
+    this.pushConnectionRegistry = pushConnectionRegistry;
+    this.pushProtocol = pushProtocol;
     this.destroyed = new AtomicBoolean();
     this.scheduledFutures = Collections.synchronizedList(new ArrayList<>());
   }
