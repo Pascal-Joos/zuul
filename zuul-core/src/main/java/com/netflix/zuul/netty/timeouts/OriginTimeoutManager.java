9b09ee09d477c7aef62e18e66cf56930733ffe74
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:15:41 +0000

    fix: 19 - UNBOX_NULLABLE - computedTimeout = originTimeout == null ? requestTimeout : originTimeout; - unboxing of a @Nullable value - originTimeout == null ? requestTimeout : originTimeout

--- zuul-core/src/main/java/com/netflix/zuul/netty/timeouts/OriginTimeoutManager.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/timeouts/OriginTimeoutManager.java
@@ -68,7 +68,9 @@ public class OriginTimeoutManager {
     if (originTimeout == null && requestTimeout == null) {
       computedTimeout = MAX_OUTBOUND_READ_TIMEOUT_MS.get();
     } else if (originTimeout == null || requestTimeout == null) {
-      computedTimeout = originTimeout == null ? requestTimeout : originTimeout;
+      Long nonNullTimeout = originTimeout != null ? originTimeout : requestTimeout;
+      computedTimeout =
+          nonNullTimeout != null ? nonNullTimeout.longValue() : MAX_OUTBOUND_READ_TIMEOUT_MS.get();
     } else {
       // return the stricter (i.e. lower) of the two timeouts
       computedTimeout = Math.min(originTimeout, requestTimeout);
