ecaec93df860e27f3382c8d630eac97f32be0d9e
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 12:20:16 +0000

    Revert "fix: 3 - RETURN_NULLABLE - return super.get(key); - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 2a2ab552d18ad8f4873babe2e3a5dbb91b58f32b.

--- zuul-core/src/main/java/com/netflix/zuul/netty/filter/BaseZuulFilterRunner.java
+++ zuul-core/src/main/java/com/netflix/zuul/netty/filter/BaseZuulFilterRunner.java
@@ -340,6 +340,7 @@ public abstract class BaseZuulFilterRunner<I extends ZuulMessage, O extends Zuul
 
   protected void recordFilterError(
       final I inMesg, final ZuulFilter<I, O> filter, final Throwable t) {
+    // Add a log statement for this exception.
     final String errorMsg =
         "Filter Exception: filter="
             + filter.filterName()
@@ -356,10 +357,9 @@ public abstract class BaseZuulFilterRunner<I extends ZuulMessage, O extends Zuul
     // Store this filter error for possible future use. But we still continue with next filter in
     // the chain.
     final SessionContext zuulCtx = inMesg.getContext();
-    final List<FilterError> filterErrors = zuulCtx.getFilterErrors();
-    if (filterErrors != null) {
-      filterErrors.add(new FilterError(filter.filterName(), filter.filterType().toString(), t));
-    }
+    zuulCtx
+        .getFilterErrors()
+        .add(new FilterError(filter.filterName(), filter.filterType().toString(), t));
     if (zuulCtx.debugRouting()) {
       Debug.addRoutingDebug(
           zuulCtx,
